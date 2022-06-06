29af864f3ac36f02f49a4903a3435f5d11ccd3a1
akshitahuja888 <akshitahuja888@gmail.com>
2022-06-06 14:47:20 +0530

    Added Controllers

---
+++ backend/controllers/adminControllers.js
@@ -0,0 +1,21 @@
+const onGroundEvents = require('../models/onGroundEvents');
+const VirtualEvents = require('../models/virtualEvents');
+const Volunteers = require('../models/volunteers');
+
+module.exports.allVirtualEvents =async(req,res) =>{
+    const allvirtualEvents = await VirtualEvents.find({});
+    console.log(allvirtualEvents);
+    res.status(200).json(allvirtualEvents);
+};
+
+module.exports.allonGroundEvents =async(req,res) =>{
+    const allonGroundEvents = await onGroundEvents.find({});
+    console.log(allonGroundEvents);
+    res.status(200).json(allonGroundEvents);
+};
+
+module.exports.allVolunteers =async(req,res) =>{
+    const allVolunteers = await Volunteers.find({});
+    console.log(allVolunteers);
+    res.status(200).json(allVolunteers);
+};
