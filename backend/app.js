ee5af2d9bb6cbf8bd480b49e5d64a250925b546a
Nitish Kumar <nitishkumar12c@gmail.com>
2022-06-11 09:22:46 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const https = require("https");
 const fs = require("fs");
-const session = require('express-session');
-const passport = require('passport');
-const localstrategy = require('passport-local');
-const cors = require('cors');
-
-
+const session = require("express-session");
+const passport = require("passport");
+const localstrategy = require("passport-local");
+const cors = require("cors");
 
 const HttpError = require("./models/http-error");
-const Volunteer=require('./models/volunteers');
+const Volunteer = require("./models/volunteers");
 
 const adminRoutes = require("./routes/adminRoutes");
 const onGroundRoutes = require("./routes/onGroundEventsRoutes");
@@ -23,18 +21,28 @@ const mappingUtil = require("./util/algo");
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  
+  next();
+});
+app.use(cors({ credentials: true, origin: true }));
 const sessionConfig = {
-  name: 'session',
-  secret:'thisisasecret',
+  name: "session",
+  secret: "thisisasecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
-      httpOnly: true,
-      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-      maxAge: 1000 * 60 * 60 * 24 * 7
-  }
-}
-app.use(cors({credentials: true, origin: true}));
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 app.use(bodyParser.json());
 app.use(session(sessionConfig));
 app.use(passport.initialize());
@@ -44,27 +52,19 @@ passport.use(new localstrategy(Volunteer.authenticate()));
 passport.serializeUser(Volunteer.serializeUser());
 passport.deserializeUser(Volunteer.deserializeUser());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-  next();
-});
-
-
 app.use("/check", (req, res, next) => {
   res.status(200);
   mappingUtil.OnGroudnmapping();
   res.json({ message: "Api server is working" });
 });
 
-app.get('/fakeuser',async(req,res)=>{
-  const volunteer=new Volunteer({name:'akshat',username:'akshat1234',email:'akshat1234@gmail.com'});
-  const newVolunteer=await Volunteer.register(volunteer,'ojha');
+app.get("/fakeuser", async (req, res) => {
+  const volunteer = new Volunteer({
+    name: "akshat",
+    username: "akshat1234",
+    email: "akshat1234@gmail.com",
+  });
+  const newVolunteer = await Volunteer.register(volunteer, "ojha");
   res.send(newVolunteer);
 });
 
