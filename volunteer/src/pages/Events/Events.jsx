26d9f97681588bc8ba3ada5a85b253bdf93e17bf
Tushar <tbharti2001@gmail.com>
2022-06-12 17:12:24 +0530

    Fetched upcoming and past events

--- volunteer/src/pages/Events/Events.jsx
+++ volunteer/src/pages/Events/Events.jsx
@@ -1,18 +1,20 @@
 import './Event.css'
 import ArrangedCards from '../../components/ArrangedCards/ArrangedCards'
 import Navbar from '../../components/Navbar/Navbar'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import axios from 'axios'
 
 export default function Events() {
     const { user } = useContext(AuthContext);
+    const [upcoming, setUpcoming] = useState([]);
+    const [past, setPast] = useState([]);
     useEffect(() => {
         console.log(user.id);
         async function fetchUpcomingEvents() {
             try {
                 const response = await axios.get(`/volunteers/upcomingEvents/${user.id}`, { withCredentials: true });
-                console.log("Upcoming: ", response.data);
+                setUpcoming(response.data);
             } catch (error) {
                 console.log(error);
             }
@@ -20,7 +22,7 @@ export default function Events() {
         async function fetchPastEvents() {
             try {
                 const response = await axios.get(`/volunteers/pastEvents/${user.id}`, { withCredentials: true });
-                console.log("Past: ", response.data);
+                setPast(response.data);
             } catch (error) {
                 console.log(error);
             }
@@ -40,14 +42,14 @@ export default function Events() {
                     <div className="heading">
                         <h1>Upcoming Events</h1>
                     </div>
-                    <ArrangedCards type="own" />
+                    <ArrangedCards type="own" list={upcoming} />
                 </div>
                 <hr style={{ width: "99%", marginTop: '50px', marginBottom: '50px' }} />
                 <div className="pastEvents">
                     <div className="heading">
                         <h1>Past Events</h1>
                     </div>
-                    <ArrangedCards type="own" />
+                    <ArrangedCards type="own" list={past} />
                 </div>
             </div>
         </div>
