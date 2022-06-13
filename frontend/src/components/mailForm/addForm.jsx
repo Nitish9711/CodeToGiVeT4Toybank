ccbf29f7f90fe38d1585e9a5f4db57435e015af2
Tushar <tbharti2001@gmail.com>
2022-06-13 15:51:28 +0530

    Integrated add volunteer manually route

--- frontend/src/components/mailForm/addForm.jsx
+++ frontend/src/components/mailForm/addForm.jsx
@@ -2,18 +2,44 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-// import axios from 'axios';
+import axios from 'axios';
 
 import "./mailForm.scss"
 
 function AddForm({ type, id, setClose }) {
     const [email, setemail] = React.useState("");
-    const handleSubmit = async () => {
+    const handleOnGroundSubmit = async () => {
         const payload = {
-            id, 
+            id,
             email
         }
-        console.log(payload, type);
+        try {
+            const response = await axios.post(`/onGroundEvents/assignVolunteer`, payload, { withCredentials: true });
+            console.log(response.data);
+            window.location.reload();
+        } catch (error) {
+            window.alert("User Not Found");
+            console.log(error);
+        }
+    }
+
+    const handleVirtualSubmit = async () => {
+        const payload = {
+            id,
+            email
+        }
+        try {
+            const response = await axios.post(`/virtualEvents/assignVolunteer`, payload, { withCredentials: true });
+            console.log(response.data);
+            window.location.reload();
+        } catch (error) {
+            window.alert("User Not Found");
+            console.log(error);
+        }
+    }
+    const handleSubmit = async () => {
+
+        type === "onGround" ? handleOnGroundSubmit() : handleVirtualSubmit();
         setClose(false);
     }
     return (
